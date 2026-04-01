d08d57e60c61bb88a4f5f2731d891dc86f8441ba
Astryx <raj.tibarewala@gmail.com>
2026-04-01 15:29:14 +0530

    Connected frontend and backend

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,13 @@ app = FastAPI()
 
 player_paths = {}
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class Location(BaseModel):
     user_id:int
     lat:float
--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,26 @@
 // Initialize map
-const map = L.map('map').setView([12.9716, 79.1588], 15); // VIT-ish coords
+const map = L.map('map').setView([12.9716, 79.1588], 15);
 
 // Add tile layer (actual map visuals)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-map.on('click', function (e) {
+map.on('click', async function (e) {
     const lat = e.latlng.lat;
     const lon = e.latlng.lng;
 
-    console.log("Clicked:", lat, lon);
+    console.log("Sending:", lat, lon);
+
+    await fetch("http://127.0.0.1:8000/update-location", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            user_id: 1,
+            lat: lat,
+            lon: lon
+        })
+    });
 });
